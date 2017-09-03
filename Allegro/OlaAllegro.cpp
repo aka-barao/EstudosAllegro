3a0f0d4ce703360fd989d96ccddeac951514da70
Vinicius Elias <viniciuselias28@hotmail.com>
2017-09-03 18:40:19 -0300

    "Olá Mundo" em uma janela do Allegro.

---
+++ Allegro/OlaAllegro.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+
+int olaallegro(int argc, char **argv) {
+	ALLEGRO_DISPLAY *display = NULL;
+
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return EXIT_FAILURE;
+	}
+
+	display = al_create_display(640, 480);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return EXIT_FAILURE;
+	}
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+	al_flip_display();
+	al_rest(10.0);
+
+	al_destroy_display(display);
+
+	return (EXIT_SUCCESS);
+}
